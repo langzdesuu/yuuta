06f96ab72803132e9aa7ab284a9b23214244ebe5
langzdesuu <118954332+langzdesuu@users.noreply.github.com>
2022-11-24 12:00:35 +0700

    Add files via upload

---
+++ dist/config/index.js
@@ -0,0 +1,54 @@
+import { lang } from "./env";
+import { Intents, Options, Sweepers } from "discord.js";
+import { join } from "path";
+import i18n from "i18n";
+export const clientOptions = {
+    allowedMentions: {
+        parse: [
+            "users"
+        ],
+        repliedUser: true
+    },
+    intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MESSAGES,
+        Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
+        Intents.FLAGS.GUILD_VOICE_STATES,
+        Intents.FLAGS.GUILD_BANS
+    ],
+    makeCache: Options.cacheWithLimits({
+        MessageManager: {
+            maxSize: Infinity,
+            sweepInterval: 300,
+            sweepFilter: Sweepers.filterByLifetime({
+                lifetime: 10800
+            })
+        },
+        ThreadManager: {
+            maxSize: Infinity,
+            sweepInterval: 300,
+            sweepFilter: Sweepers.filterByLifetime({
+                lifetime: 10800,
+                getComparisonTimestamp: (e)=>e.archiveTimestamp,
+                excludeFromSweep: (e)=>!e.archived
+            })
+        }
+    }),
+    retryLimit: 3
+};
+i18n.configure({
+    defaultLocale: "en",
+    directory: join(process.cwd(), "lang"),
+    locales: [
+        "en",
+        "es",
+        "id",
+        "fr"
+    ],
+    objectNotation: true
+});
+i18n.setLocale(lang);
+export const shardsCount = "auto";
+export const shardingMode = "worker";
+export * from "./env";
+export default i18n;
